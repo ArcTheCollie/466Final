df61ee3228ce651af046cc95fd0c99a5f00e1fa3
Stephen Daily <stephendaily@Stephens-MacBook-Air.local>
2016-12-12 12:25:36 -0800

    Finished implementing the rest of Vector

--- Vector.java
+++ Vector.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.ArrayList; 
 
 public class Vector implements Comparable<Vector> {
 	ArrayList<Integer> points;
@@ -15,7 +15,13 @@ public class Vector implements Comparable<Vector> {
 	
 	/*Write a function that takes in a vector and returns the distance.*/
 	public void computeDistance(ArrayList<Integer> vectorB) {
-		
+		if(vectorB.size() != points.size())
+			System.out.println("Something is wrong with the vector sizes.");
+		Double dist = 0.0;
+		for(int i = 0; i < points.size(); i++) {
+			dist += Math.pow(points.get(i) - vectorB.get(i), 2);
+		}
+		distance = Math.sqrt(dist);
 	}
 	
 	/*Implement a comparator on distance*/
